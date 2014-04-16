d9e93ae7ea3487ea3ab2ee8bf7fc4c98227c6573
tux <sena.ufc@gmail.com>
2014-04-16 10:47:02 -0300

    prova 2

---
+++ prova2/aluno.cpp
@@ -0,0 +1,62 @@
+//PROVA 2
+#include <iostream>
+using namespace std;
+
+//contar e retornar quantos elementos são menores que a media
+int qtd_contar_menores_media(float *vet, int tam){
+    return 0;
+}
+
+bool eh_vogal(char letra){
+    return false;
+}
+
+bool eh_consoante(char letra){
+    return false;
+}
+
+//retornar a soma dos primeiros n numeros que sao divisiveis por 2 e por 5 mais não sao por 3
+int soma_primeiros_25n3(){
+    return 0;
+}
+
+//contar quantos caracters unicos existem no nome
+int contar_unicos(char *nome){
+    return 0;
+}
+
+/*   VERSAO HARD */
+
+//colocar em qtd quantos vezes a menor aparece na maior e em
+//pos as posições
+void substring(char *maior, char *menor, int &qtd, int pos[]){
+
+}
+
+
+//repetir codigo ate encher o texto
+//cifrar fazendo soma modular de char
+//ex : codigo= "bia";
+//ex : texto = "batatinha doce"
+//              biabiabiabiabi
+//   fazer soma somular um a um
+//
+//   colocar a resposta em resposta
+void cifra(char *texto, char *codigo, char *resposta){
+
+}
+
+
+
+//fazer o codigo que decifra a cifra acima
+void decifra(char *cifrado, char *codigo, char *resposta){
+
+}
+
+
+
+
+
+
+
+
