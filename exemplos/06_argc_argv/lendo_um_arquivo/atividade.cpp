723c8ed527493f0ae2ad83e959d41a6f76bc7b99
David <sena.ufc@gmail.com>
2014-05-14 11:06:49 -0300

    up

---
+++ exemplos/06_argc_argv/lendo_um_arquivo/atividade.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+//ler um parametro que corresponde ao nome do arquivo,
+//abre o arquivo passado por parametro
+//lê as linhas e imprime na tela.
+int main(){
+    return 0;
+}
+
---
+++ exemplos/06_argc_argv/lendo_um_arquivo/ler_e_escrever.cpp
@@ -0,0 +1,27 @@
+//http://shelr.tv/records/5373796a9660806742000021
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+//ler um parametro que corresponde ao nome do arquivo,
+//abre o arquivo passado por parametro
+//lê as linhas e imprime na tela.
+int main(int argc, char ** argv){
+    if(argc < 2){
+        cout << "Olha filho, tem que passar outro parametro\n";
+        return 1;
+    }
+    //argv[0] nome do programa
+    //argv[1]
+    ifstream entrada(argv[1]);
+
+    string linha;
+    while(entrada){
+        getline(entrada, linha);
+        cout << linha << endl;
+    }
+    return 0;
+}
+
