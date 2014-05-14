17609055ecc7d78b839a41f7ce16208bc8f8a066
David <sena.ufc@gmail.com>
2014-05-14 10:39:28 -0300

    up

--- exemplos/05_arquivos/arquivos.cpp
+++ exemplos/05_arquivos/arquivos.cpp
@@ -1,15 +1,24 @@
+//http://shelr.tv/records/53737307966080674200001f
 #include <iostream>
 using namespace std;
-
+//para mudar a entrada padrao eh
+//./a.out < input.txt
+//
 //./executavel < input.txt > numeros.txt
 
 int main(){
-    cout << "de quanto a quanto voce quer mostrar";
+    cout << "de quanto a quanto voce quer mostrar" << endl;
     int inicio = 0;
     int fim = 0;
     cin >> inicio;
     cin >> fim;
-    for(int i = inicio; i < fim; i++)
-        cout << i << endl;
+    //for(int i = inicio; i < fim; i++)
+        //cout << i << endl;
+    string nome;
+    getline(cin, nome);
+    getline(cin, nome);
+    cout << "meu nome " << nome << endl;
+
+
     return 0;
 }
