d06568cf97924aaad075e8ec648cd8d46757fc6e
David <sena.ufc@gmail.com>
2014-05-14 10:50:05 -0300

    up

--- exemplos/05_arquivos/criando_descritores/arquivos.cpp
+++ exemplos/05_arquivos/criando_descritores/arquivos.cpp
@@ -1,27 +1,33 @@
+//http://shelr.tv/records/537375779660806742000020
+
 #include <iostream>
 #include <fstream> //para trabalhar com arquivos
 #include <string>
 using namespace std;
-
-
-//./executavel < input.txt > numeros.txt
 
 int main(){
     cout << "de quanto a quanto voce quer mostrar \n";
     int inicio = 0;
     int fim = 0;
     string nome;
+    string time;
 
+    //input file stream
     ifstream arq_input ("input.txt");
+    //output file stream
     ofstream arq_saida ("numeros.txt");
 
     arq_input >> inicio;
     arq_input >> fim;
     getline(arq_input, nome);
-    getline(arq_input, nome);
-
+    getline(arq_input, time);
+    arq_saida << inicio << endl;
+    arq_saida << fim << endl;
     arq_saida << nome << endl;
-    arq_saida << "meteoro de pegasus" << endl;
+    arq_saida << time << endl;
+
+   // arq_saida << nome << endl;
+    //arq_saida << "meteoro de pegasus" << endl;
 
     return 0;
 }
