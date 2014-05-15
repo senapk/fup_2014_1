878f7936c4fe568a048330ef7d6c2438c213b4d9
David <sena.ufc@gmail.com>
2014-05-15 09:55:33 -0300

    up

--- exemplos/07_jogo_forca/jogo_forca.cpp
+++ exemplos/07_jogo_forca/jogo_forca.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "tk.h"
 using namespace std;
 
 int main(){
+    tk_color('b');
+    tk_write(5,5,"batatinha");
+    tk_color('m');
+    tk_write(10, 10, "pitombas");
+    char c = tk_wait();
+    tk_color('w');
+    tk_write(20, 20, "%c", c);
+
+    const char *cores = tk_info_colors();
+    tk_move(5,5);
+    for(int i = 0; i < (int)strlen(cores); i++)
+    {
+        tk_color(cores[i]);
+        cout << "oi";
+        //tk_write(4, i, "opa");
+    }
+
+    tk_wait();
     return 0;
 }
 
